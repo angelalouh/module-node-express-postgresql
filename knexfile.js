a6ab86affecee265cf4dd3e32fdec6447bc60d5c
angelalouh <louh.angela@gmail.com>
2021-10-18 15:25:00 -0700

    Updated file to indicate where to store seed scripts

--- knexfile.js
+++ knexfile.js
@@ -24,5 +24,8 @@ module.exports = {
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
   },
 };
