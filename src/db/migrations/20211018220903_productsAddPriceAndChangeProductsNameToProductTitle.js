6f0d78be282d502d42b4b1e0ebf4542977afdc07
angelalouh <louh.angela@gmail.com>
2021-10-18 15:12:15 -0700

    Creating new migration to rename column and add new column

---
+++ src/db/migrations/20211018220903_productsAddPriceAndChangeProductsNameToProductTitle.js
@@ -0,0 +1,13 @@
+exports.up = function (knex) {
+  return knex.schema.table("products", (table) => {
+    table.renameColumn("products_name", "product_title");
+    table.decimal("product_price"); // Adding a new column
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.table("products", (table) => {
+    table.renameColumn("product_title", "product_name");
+    table.dropColumn("product_price");
+  });
+};
