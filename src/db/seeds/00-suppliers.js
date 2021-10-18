7146310c96bbf159216dc72abfee0b3a8e15b68d
angelalouh <louh.angela@gmail.com>
2021-10-18 15:57:10 -0700

    Customized the four seed files

--- src/db/seeds/00-suppliers.js
+++ src/db/seeds/00-suppliers.js
@@ -1,13 +1,13 @@
+// Requiring the suppliers seed data and stores it in the suppliers variable
+const suppliers = require("../fixtures/suppliers");
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+exports.seed = function (knex) {
+  return knex
+    // The knex.raw() method uses the SQL statement RESTART IDENTITY to reset the primary key values
+    // Adding CASCADE ensures any references to the entries in the suppliers table are deleted as well when the supplier entries are deleted
+    .raw("TRUNCATE TABLE suppliers RESTART IDENTITY CASCADE")
     .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+      // This line will only get executed after the preceding knex.raw() function is complete
+      return knex("suppliers").insert(suppliers);
     });
 };
