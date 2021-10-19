88978461dda409a19fe04ef270b8dccaf31c1322
angelalouh <louh.angela@gmail.com>
2021-10-19 15:50:16 -0700

    Updated list function to make use of async and await

--- src/categories/categories.controller.js
+++ src/categories/categories.controller.js
@@ -2,6 +2,12 @@
 const categoriesService = require("./categories.service");
 
 async function list(req, res, next) {
+  // categoriesService.list() executes a Knex query, which is an async operation
+  // Using await forces the execution of the code to pause on that line until that async operation is finished
+  const data = await categoriesService.list();
+  res.json({ data });
+
+  /* Code using then() and catch()
   categoriesService
     // Accessing the list method to perform CRUD operations on a table
     .list()
@@ -9,6 +15,7 @@ async function list(req, res, next) {
     .then((data) => res.json({ data }))
     // Chaining catch(next) onto the promise will call next() passing in the error
     .catch(next);
+  */
 }
 
 module.exports = {
