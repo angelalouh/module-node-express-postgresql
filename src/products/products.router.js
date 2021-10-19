c04dd03d284093afeec078765e534a181c6e4cf6
angelalouh <louh.angela@gmail.com>
2021-10-19 14:13:35 -0700

    Validated :movieId route param using regex

--- src/products/products.router.js
+++ src/products/products.router.js
@@ -3,6 +3,8 @@ const controller = require("./products.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router.route("/").get(controller.list).all(methodNotAllowed);
-router.route("/:productId").get(controller.read).all(methodNotAllowed);
+
+// regex expression, "([0-9]+)",  ensures the route param, :movieId, consists of just one or more digits
+router.route("/:productId([0-9]+)").get(controller.read).all(methodNotAllowed);
 
 module.exports = router;
