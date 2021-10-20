144491d5431ba129d475bac9b70de41b5d923010
angelalouh <louh.angela@gmail.com>
2021-10-20 00:47:43 -0700

    Modified existing read() query builder function to also return a product's related category info

--- src/products/products.service.js
+++ src/products/products.service.js
@@ -1,17 +1,19 @@
 const knex = require("../db/connection");
 
 function listTotalWeightByProduct() {
-  return knex("products")
-    // This query selects 3 columns
-    .select(
-      "product_sku",
-      "product_title",
-      // This is the 3rd column, which is the sum of multiplying the values from 2 columns.
-      knex.raw(
-        "sum(product_weight_in_lbs * product_quantity_in_stock) as total_weight_in_lbs"
+  return (
+    knex("products")
+      // This query selects 3 columns
+      .select(
+        "product_sku",
+        "product_title",
+        // This is the 3rd column, which is the sum of multiplying the values from 2 columns.
+        knex.raw(
+          "sum(product_weight_in_lbs * product_quantity_in_stock) as total_weight_in_lbs"
+        )
       )
-    )
-    .groupBy("product_title", "product_sku");
+      .groupBy("product_title", "product_sku")
+  );
 }
 
 function listPriceSummary() {
@@ -35,11 +37,22 @@ function list() {
   return knex("products").select("*");
 }
 
-/* This read() fxn creates a Knex query that selects all columns from the products table
-    where the product_id column matches the argument passed to the read() fxn. The first() 
-    method returns the first row in the table as an object. */
 function read(product_id) {
+  // Will return a specific product, including all of its related category information
+  return knex("products as p")
+    .join("products_categories as pc", "p.product_id", "pc.product_id")
+    .join("categories as c", "pc.category_id", "c.category_id")
+    .select("p.*", "c.*")
+    .where({ "p.product_id": product_id })
+    .first();
+
+  /* This code creates a Knex query that selects all columns from the products table
+    where the product_id column matches the argument passed to the read() fxn. The first() 
+    method returns the first row in the table as an object.
+
   return knex("products").select("*").where({ product_id }).first();
+
+  */
 }
 
 module.exports = {
