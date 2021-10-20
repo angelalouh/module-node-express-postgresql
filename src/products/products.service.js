a2c731edc62ebe83bc79a1f2999e207f9495d4b9
angelalouh <louh.angela@gmail.com>
2021-10-19 23:57:38 -0700

    Added listPriceSummary query function

--- src/products/products.service.js
+++ src/products/products.service.js
@@ -1,26 +1,36 @@
 const knex = require("../db/connection");
 
+function listPriceSummary() {
+  return knex("products")
+    .select("supplier_id")
+    .min("product_price")
+    .max("product_price")
+    .avg("product_price")
+    .groupBy("supplier_id");
+}
+
 function listOutOfStockCount() {
-    return knex("products")
-        .select("product_quantity_in_stock as out_of_stock")
-        .count("product_id")
-        .where({ product_quantity_in_stock: 0 })
-        .groupBy("out_of_stock");
+  return knex("products")
+    .select("product_quantity_in_stock as out_of_stock")
+    .count("product_id")
+    .where({ product_quantity_in_stock: 0 })
+    .groupBy("out_of_stock");
 }
 
 function list() {
-    return knex("products").select("*");
+  return knex("products").select("*");
 }
 
 /* This read() fxn creates a Knex query that selects all columns from the products table
     where the product_id column matches the argument passed to the read() fxn. The first() 
     method returns the first row in the table as an object. */
 function read(product_id) {
-    return knex("products").select("*").where({ product_id }).first();
+  return knex("products").select("*").where({ product_id }).first();
 }
 
 module.exports = {
-    list,
-    read,
-    listOutOfStockCount,
+  list,
+  read,
+  listOutOfStockCount,
+  listPriceSummary,
 };
