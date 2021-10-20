46337078d564bb3a5df5c3a7d5c5704863bf3cb3
angelalouh <louh.angela@gmail.com>
2021-10-20 12:59:36 -0700

    Modified the read() to create a nested category obj using mapProperties()

--- src/products/products.service.js
+++ src/products/products.service.js
@@ -1,4 +1,5 @@
 const knex = require("../db/connection");
+const mapProperties = require("../utils/map-properties");
 
 function listTotalWeightByProduct() {
   return (
@@ -37,6 +38,16 @@ function list() {
   return knex("products").select("*");
 }
 
+// Creating nested category object for the data object returned by read()
+  // The values in the configuration obj below specify the new property name. If a portion of the path doesn't exist, 
+  // which in this case, the category object doesn't exist yet, it will be created.
+  // Any property not in the configuration is left unchanged
+const addCategory = mapProperties({
+  category_id: "category.category_id",
+  category_name: "category.category_name",
+  category_description: "category.category_description",
+});
+
 function read(product_id) {
   // Will return a specific product, including all of its related category information
   return knex("products as p")
@@ -44,7 +55,8 @@ function read(product_id) {
     .join("categories as c", "pc.category_id", "c.category_id")
     .select("p.*", "c.*")
     .where({ "p.product_id": product_id })
-    .first();
+    .first()
+    .then(addCategory);
 
   /* This code creates a Knex query that selects all columns from the products table
     where the product_id column matches the argument passed to the read() fxn. The first() 
