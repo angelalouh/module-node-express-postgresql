debd1c1162fc130cfbe8606bfa4233de637b9535
angelalouh <louh.angela@gmail.com>
2021-10-19 19:07:18 -0700

    Added out-of-stock route to router file

--- src/products/products.router.js
+++ src/products/products.router.js
@@ -2,6 +2,8 @@ const router = require("express").Router({ mergeParams: true });
 const controller = require("./products.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
+router.route("/out-of-stock-count").get(controller.listOutOfStockCount).all(methodNotAllowed);
+
 router.route("/").get(controller.list).all(methodNotAllowed);
 
 // regex expression, "([0-9]+)",  ensures the route param, :movieId, consists of just one or more digits
